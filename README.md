cdf1f2d3a15f3022dceb3ec7e4235762b98d2352
devemanager <devemanager@gmail.com>
2020-04-29 14:51:03 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-odata-data-source-aagpba
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-odata-data-source-aagpba)
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import {
+  MatTableModule,
+  MatSortModule,
+  MatPaginatorModule
+} from '@angular/material';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
